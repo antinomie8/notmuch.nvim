715223c4a95504ed2dc6df98b161b2b4eb3855d2
Yousef Akbar <yhakbar99@gmail.com>
2026-01-16 16:15:37 +0300

    feat(thread): export thread metadata to buffer-local variable
    Add `vim.b.notmuch_thread` buffer variable containing thread-level
    metadata for extensibility (statusline integration, custom scripts).
    
    Exported fields:
    - id: Thread ID for notmuch queries
    - subject: Thread subject from root message
    - date_relative: Root message date
    - message_count: Total messages in thread
    - tags: Union of all message tags (sorted)
    - authors: Unique participants (full From header, deduplicated)
    
    Implementation:
    - Accumulate metadata during `build_message_lines()` tree traversal
    - Return (lines, metadata) tuple from `show_thread()`
    - Caller sets buffer variable after creating buffer
    
    This is the first of several buffer variables that will enable
    O(1) message lookup and rich statusline integration.

--- lua/notmuch/init.lua
+++ lua/notmuch/init.lua
@@ -166,8 +166,9 @@ nm.show_thread = function(s)
   v.nvim_buf_set_name(buf, "thread:" .. threadid)
   v.nvim_win_set_buf(0, buf)
 
-  local lines = require('notmuch.thread').show_thread(threadid)
+  local lines, metadata = require('notmuch.thread').show_thread(threadid)
   v.nvim_buf_set_lines(buf, 0, -1, false, lines)
+  vim.b.notmuch_thread = metadata
 
   -- Insert hint message at the top of the buffer
   local hint_text = "Hints: <Enter>: Toggle fold message | <Tab>: Next message | <S-Tab>: Prev message | q: Close | a: See attachment parts"
