8b645c3f5839779856829df04b7d2ee8a67e63e8
Antoine Saez Dumas <antoine.saezdumas.git@gmail.com>
2026-04-05 11:41:15 +0200

    fix(ftplugin): fix typo in require

--- ftplugin/notmuch-attach.lua
+++ ftplugin/notmuch-attach.lua
@@ -1,4 +1,4 @@
 vim.keymap.set("n", "q", function() require("notmuch.util").quit_or_bwipeout() end, { buffer = true })
-vim.keymap.set("n", "v", function() require("nomtuch.attach").view_attachment_part() end, { buffer = true })
-vim.keymap.set("n", "o", function() require("nomtuch.attach").open_attachment_part() end, { buffer = true })
+vim.keymap.set("n", "v", function() require("notmuch.attach").view_attachment_part() end, { buffer = true })
+vim.keymap.set("n", "o", function() require("notmuch.attach").open_attachment_part() end, { buffer = true })
 vim.keymap.set("n", "s", function() require("notmuch.attach").save_attachment_part(nil, true) end, { buffer = true })
